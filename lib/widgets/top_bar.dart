65aebff543a54c879e0df3a41ce82f09376f8689
asr <asr12211@outlook.com>
2023-12-07 22:12:24 +0300

    FIX: topBar overflow on open

--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -7,16 +7,14 @@ import "theme_selection.dart";
 
 class TopBar extends StatefulWidget {
   const TopBar({
-    super.key, 
-    required this.pageTitle, 
-    required this.isOpen, 
-    required this.height, 
+    super.key,
+    required this.pageTitle,
+    required this.isOpen,
     required this.setHeight
   });
 
   final String pageTitle;
   final bool isOpen;
-  final double height;  // may be deleted
   final Function() setHeight;
 
   @override
@@ -25,74 +23,111 @@ class TopBar extends StatefulWidget {
 
 class _TopBarState extends State<TopBar> {
 
-  // List<Widget> topBarContent = [];
+  List<Widget> topBarContent = [];
 
-  // @override
-  // void didChangeDependencies() {
+  @override
+  void initState() {
+    super.initState();
 
-  //   if (!widget.isOpen) {
+    topBarContent = [
+      const SizedBox(height: 50,),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {
+              widget.setHeight();
+            },
+            icon: Icon(widget.isOpen ? Icons.close_rounded : Icons.menu, color: colors.lightThemeTextColor,)
+          ),
+          Text(widget.pageTitle, style: const TextStyle(fontSize: 32, color: colors.lightThemeTextColor),),
+          const SizedBox(width: 50),
+        ]
+      ),
+    ];
+  }
 
-  //     Future.delayed(Duration(milliseconds: 100), () {
-  //       setState(() {
-  //         topBarContent = [
-  //           const SizedBox(height: 40,),
-  //           Row(
-  //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //             children: [
-  //               IconButton(
-  //                 onPressed: () {
-  //                   widget.setHeight();
-  //                 },
-  //                 icon: const Icon(Icons.menu, color: colors.lightThemeTextColor,)
-  //               ),
-  //               Text(widget.pageTitle, style: const TextStyle(fontSize: 32, color: colors.lightThemeTextColor),),
-  //               const SizedBox(width: 50),
-  //             ]
-  //           ),
-  //           const SizedBox(height: 30,),
-  //           const Row(children: [
-  //             Text("Base Currency", style: const TextStyle(fontSize: 18, color: colors.lightThemeTextColor),),
-  //             // BaseDropdown(),
-  //           ],),
-  //           const SizedBox(height: 5,),
-  //           const Row(children: [
-  //             Text("Theme", style: const TextStyle(fontSize: 18, color: colors.lightThemeTextColor),),
-  //           ],),
-  //           const SizedBox(height: 5,),
-  //           GestureDetector(
-  //             onTap: () {},
-  //             child: Container(
-  //               alignment: Alignment.centerLeft,
-  //               child: const Text("About App", style: const TextStyle(fontSize: 18, color: colors.lightThemeTextColor),),
-  //             ),
-  //           ),
-  //         ];
-  //       });
-  //     });
-  //   } else {
+  @override
+  void didUpdateWidget(TopBar oldWidget) {
 
-  //     setState(() {
-  //       topBarContent = [
-  //         const SizedBox(height: 40,),
-  //         Row(
-  //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //           children: [
-  //             IconButton(
-  //               onPressed: () {
-  //                 widget.setHeight();
-  //               },
-  //               icon: const Icon(Icons.menu, color: colors.lightThemeTextColor,)
-  //             ),
-  //             Text(widget.pageTitle, style: const TextStyle(fontSize: 32, color: colors.lightThemeTextColor),),
-  //             const SizedBox(width: 50),
-  //           ]
-  //         ),
-  //       ];
-  //     });
+    if (widget.isOpen) {
+
+      Future.delayed(const Duration(milliseconds: 150), () {
+        setState(() {
+          topBarContent = [
+            const SizedBox(height: 50,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    widget.setHeight();
+                  },
+                  icon: Icon(widget.isOpen ? Icons.close_rounded : Icons.menu, color: colors.lightThemeTextColor,)
+                ),
+                Text(widget.pageTitle, style: const TextStyle(fontSize: 32, color: colors.lightThemeTextColor),),
+                const SizedBox(width: 50),
+              ]
+            ),
+            const SizedBox(height: 30,),
+            Row(children: [
+              const SizedBox(width: 9,),
+              SizedBox(width: 30, child: Image.asset("images/coin.png")),
+              const SizedBox(width: 13,),
+              const Text("Base Currency:", style: TextStyle(fontSize: 16, color: colors.lightThemeTextColor),),
+              const SizedBox(width: 10,),
+              const BaseDropdown(),
+            ],),
+            const SizedBox(height: 20,),
+            Row(children: [
+              const SizedBox(width: 12,),
+              SizedBox(width: 25, child: Image.asset("images/theme.png")),
+              const SizedBox(width: 15,),
+              const Text("Theme:", style: TextStyle(fontSize: 16, color: colors.lightThemeTextColor),),
+              const ThemeSelection(),
+            ],),
+            const SizedBox(height: 20,),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  children: [
+                    const SizedBox(width: 12,),
+                    SizedBox(width: 25, child: Image.asset("images/about.png")),
+                    const SizedBox(width: 15,),
+                    const Text("About App", style: TextStyle(fontSize: 16, color: colors.lightThemeTextColor),),
+                  ],
+                ),
+              ),
+            ),
+          ];
+        });
+      });
+    } else {
+
+      setState(() {
+        topBarContent = [
+          const SizedBox(height: 50,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: () {
+                  widget.setHeight();
+                },
+                icon: Icon(widget.isOpen ? Icons.close_rounded : Icons.menu, color: colors.lightThemeTextColor,)
+              ),
+              Text(widget.pageTitle, style: const TextStyle(fontSize: 32, color: colors.lightThemeTextColor),),
+              const SizedBox(width: 50),
+            ]
+          ),
+        ];
+      });
       
-  //   }
-  //   super.didChangeDependencies();
-  // }
+    }
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -108,7 +143,6 @@ class _TopBarState extends State<TopBar> {
           backgroundColor: Colors.transparent,
           flexibleSpace: Container(
             clipBehavior: Clip.antiAlias,
-            // padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
             decoration: const BoxDecoration(
               borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(15),
@@ -116,54 +150,7 @@ class _TopBarState extends State<TopBar> {
               ),
               gradient: colors.topNavGradient,
             ),
-            child: Column(children: [
-              const SizedBox(height: 50,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      widget.setHeight();
-                    },
-                    icon: Icon(widget.isOpen ? Icons.close_rounded : Icons.menu, color: colors.lightThemeTextColor,)
-                  ),
-                  Text(widget.pageTitle, style: const TextStyle(fontSize: 32, color: colors.lightThemeTextColor),),
-                  const SizedBox(width: 50),
-                ]
-              ),
-              if (widget.isOpen) const SizedBox(height: 30,),
-              if (widget.isOpen) Row(children: [
-                const SizedBox(width: 8,),
-                SizedBox(width: 30, child: Image.asset("images/coin.png")),
-                const SizedBox(width: 13,),
-                const Text("Base Currency:", style: TextStyle(fontSize: 16, color: colors.lightThemeTextColor),),
-                const SizedBox(width: 10,),
-                const BaseDropdown(),
-              ],),
-              if (widget.isOpen) const SizedBox(height: 20,),
-              if (widget.isOpen) Row(children: [
-                const SizedBox(width: 12,),
-                SizedBox(width: 25, child: Image.asset("images/theme.png")),
-                const SizedBox(width: 15,),
-                const Text("Theme:", style: TextStyle(fontSize: 16, color: colors.lightThemeTextColor),),
-                const ThemeSelection(),
-              ],),
-              if (widget.isOpen) const SizedBox(height: 13,),
-              if (widget.isOpen) GestureDetector(
-                onTap: () {},
-                child: Container(
-                  alignment: Alignment.centerLeft,
-                  child: Row(
-                    children: [
-                      const SizedBox(width: 12,),
-                      SizedBox(width: 25, child: Image.asset("images/about.png")),
-                      const SizedBox(width: 15,),
-                      const Text("About App", style: TextStyle(fontSize: 16, color: colors.lightThemeTextColor),),
-                    ],
-                  ),
-                ),
-              ),
-            ]),
+            child: Column(children: topBarContent),
           ),
         )
     );
