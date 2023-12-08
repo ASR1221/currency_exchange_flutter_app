fa3730f275e1657507467761623f576b1b7cb63e
asr <asr12211@outlook.com>
2023-12-08 22:32:53 +0300

    ADD: news page

--- lib/pages/news_details_page.dart
+++ lib/pages/news_details_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:final_project/widgets/small_top_bar.dart';
-import 'package:final_project/widgets/background_gradient_wrapper.dart';
+
+import '../widgets/small_top_bar.dart';
+import '../widgets/background_gradient_wrapper.dart';
+import '../constants/colors.dart' as colors;
 
 class NewsDetailsPage extends StatefulWidget {
   const NewsDetailsPage({super.key});
@@ -25,7 +27,31 @@ class _NewsDetailsPageState extends State<NewsDetailsPage> {
         child: SmallTopBar(changePage: changePage,),
       ),
       body: BackGroundGradientWrapper(
-        childApp: Center(child: Text("Not implemented yet"),),
+        childApp: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: ListView(
+            children: [
+              Container(
+                height: 180,
+                width: double.infinity,
+                clipBehavior: Clip.antiAlias,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  gradient: colors.loadingPlaceholderGradient,
+                ),
+                // child: Image.network(""),
+              ),
+
+              SizedBox(height: 20,),
+
+              Text("Title", style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),),
+
+              SizedBox(height: 15,),
+
+              Text("This is the body of the new where all the news are going to be displayed. It is goin to look amaizing.\n\nSo what is the latest new so we can tell you every thing.", style: TextStyle(fontSize: 16),),
+            ],
+          ),
+        ),
       ),
     );
   }
