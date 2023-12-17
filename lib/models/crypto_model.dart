74bc41f921d43ab77c67d40126756b5df4159a2d
asr <asr12211@outlook.com>
2023-12-17 16:07:31 +0300

    ADD: crypto model

---
+++ lib/models/crypto_model.dart
@@ -0,0 +1,165 @@
+import "dart:convert";
+
+import "package:http/http.dart" as http;
+
+class CryptoModel {
+
+  static const _baseURL = "https://rest.coinapi.io/v1/APIKEY-3264E81B-B4F9-45A3-A41E-92ADF08B9F3E";
+
+  static const List<String> _allCryptos = ['BTC', 'ETH', 'USDT', 'BNB', 'XRP', 'SOL', 'USDC', 'ADA', 'DOGE', 'AVAX', 'TRX', 'DOT', 'LINK', 'MATIC', 'TON'];
+  static const List<String> _allBases = ['USD', 'EUR', 'JPY', 'GBP', 'AUD', 'CAD', 'CHF', 'CNY', 'SEK', 'NZD'];
+
+  final List<Map<String, String>> _allCryptosIconsPair = [];
+  final List<Map<String, String>> _allBaseIconsPair = [];
+
+  List<Map<String, String>> get allCryptosIconsPair => _allCryptosIconsPair;
+  List<Map<String, String>> get allBaseIconsPair => _allBaseIconsPair;
+
+  void setIconPairs() async {
+
+    http.Response response = await http.get(Uri.parse("$_baseURL/icons/50"));
+
+    if (response.statusCode != 200) {
+      return;
+    }
+
+    List<Map<String, dynamic>> iconData = await jsonDecode(response.body);
+
+    for (var icon in iconData) {
+      if (_allCryptos.contains(icon['asset_id'])) {
+        _allCryptosIconsPair.add({
+          "asset_id": icon['asset_id'],
+          "icon_url": icon['url'],
+        });
+        break;
+      } else if (_allBases.contains(icon['asset_id'])) {
+        _allBaseIconsPair.add({
+          "asset_id": icon['asset_id'],
+          "icon_url": icon['url'],
+        });
+        break;
+      }
+    }
+  }
+
+  Future<dynamic> getCurrenciesBased({required String baseFiat, List<String> cryptoList = _allCryptos}) async {
+
+    http.Response response = await http.get(Uri.parse("$_baseURL/exchangerate/$baseFiat"));
+
+    if (response.statusCode != 200) {
+      return null;
+    }
+
+    Map<String, dynamic> data = await jsonDecode(response.body);
+
+    List<Map<String, dynamic>> currencies = [];
+
+    for (var asset in data['rates']) {
+      if (cryptoList.contains(asset['asset_id_quote'])) {
+        currencies.add({
+          'asset_id': asset['asset_id_quote'],
+          'rate': asset['rate'],
+        });
+      }
+    }
+
+    return currencies;
+  }
+
+  Future<dynamic> convertCurrencies({required String from, required String to, required double amount}) async {
+    http.Response response = await http.get(Uri.parse("$_baseURL/exchangerate/$from/$to"));
+
+    if (response.statusCode != 200) {
+      return null;
+    }
+
+    Map<String, dynamic> data = await jsonDecode(response.body);
+
+    return data['rate'] * amount;
+  }
+
+  Future<dynamic> historicalData({required String from, required String to, required String timeAmount}) async {
+    String endDate = DateTime.now().toIso8601String();
+    String startDate = '';
+    String periodId = '';
+
+    switch (timeAmount) {
+      case 'hour':
+        startDate = DateTime.now().subtract(const Duration(hours: 1)).toIso8601String();
+        periodId = '3MIN';
+        break;
+
+      case 'day':
+        startDate = DateTime.now().subtract(const Duration(days: 1)).toIso8601String();
+        periodId = '2HRS';
+        break;
+
+      case 'week':
+        startDate = DateTime.now().subtract(const Duration(days: 7)).toIso8601String();
+        periodId = '12HRS';
+        break;
+
+      case 'month':
+        startDate = DateTime.now().subtract(const Duration(days: 30)).toIso8601String();
+        periodId = '2DAY';
+        break;
+
+      case 'year':
+        startDate = DateTime.now().subtract(const Duration(days: 365)).toIso8601String();
+        periodId = '10DAY';
+        break;
+
+      default:
+        startDate = DateTime.now().subtract(const Duration(hours: 1)).toIso8601String();
+        periodId = '3MIN';
+        break;
+    }
+
+    http.Response response = await http.get(Uri.parse("$_baseURL/exchangerate/$from/$to/history?period_id=$periodId&time_start=$startDate&time_end=$endDate"));
+
+    if (response.statusCode != 200) {
+      return null;
+    }
+
+    List<Map<String, dynamic>> data = await jsonDecode(response.body);
+
+    switch (timeAmount) {
+      case 'hour':
+        return data.map((e) => {
+          'date': DateTime.parse(e['time_open']).minute + 1,
+          'rate': e['rate_open'],
+        });
+
+      case 'day':
+        return data.map((e) => {
+          'date': DateTime.parse(e['time_open']).hour + 1,
+          'rate': e['rate_open'],
+        });
+
+      case 'week':
+        return data.map((e) => {
+          'date': DateTime.parse(e['time_open']).weekday,
+          'rate': e['rate_open'],
+        });
+
+      case 'month':
+        return data.map((e) => {
+          'date': DateTime.parse(e['time_open']).day,
+          'rate': e['rate_open'],
+        });
+
+      case 'year':
+        return data.map((e) => {
+          'date': DateTime.parse(e['time_open']).month,
+          'rate': e['rate_open'],
+        });
+
+      default:
+        return data.map((e) => {
+          'date': DateTime.parse(e['time_open']).minute + 1,
+          'rate': e['rate_open'],
+        });
+    }
+  }
+
+}
