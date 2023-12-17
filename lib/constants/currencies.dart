11a0c5aa2f3d3d868565a34a4f6a6ada102ee4b8
asr <asr12211@outlook.com>
2023-12-17 18:52:15 +0300

    ADD: constant currencies list file

---
+++ lib/constants/currencies.dart
@@ -0,0 +1,3 @@
+const List<String> allCryptos = ['BTC', 'ETH', 'USDT', 'DOGE', 'TRX', 'DOT', 'LINK', 'STONE', 'LTC', 'DAI', 'BCH', 'ATOM'];
+
+const List<String> allBases = ['USD', 'EUR', 'GBP', 'AUD', 'CAD'];
