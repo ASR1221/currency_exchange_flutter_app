bde462ac3b659a5c8418cd2fc21725415204b99c
asr <asr12211@outlook.com>
2023-12-17 18:53:10 +0300

    ADD: base currency in provider

--- lib/provider/provider_controller.dart
+++ lib/provider/provider_controller.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProviderController extends ChangeNotifier {
@@ -7,15 +6,19 @@ class ProviderController extends ChangeNotifier {
   ProviderController(this._prefs) {
     // load theme from preferences on initialization
     _currentThemeId = _prefs.getInt("themeId") ?? 0;
+    _baseCurrency = _prefs.getString("baseCurrency") ?? "USD";
   }
 
   final SharedPreferences _prefs;
 
   // themes: 0 == system, 1 == light, 2 == dark
   int _currentThemeId = 0;
+  String _baseCurrency = "";
 
-  // get the current theme
+  // getters
   int get currentThemeId => _currentThemeId;
+
+  String get baseCurrency => _baseCurrency;
 
   void setTheme(int theme) {
     _currentThemeId = theme;
@@ -25,6 +28,12 @@ class ProviderController extends ChangeNotifier {
 
     // store updated theme on disk
     _prefs.setInt("themeId", theme);
+  }
+
+  void setBaseCurrency(String newBase) {
+    _baseCurrency = newBase;
+    notifyListeners();
+    _prefs.setString("baseCurrency", _baseCurrency);
   }
 }
 
